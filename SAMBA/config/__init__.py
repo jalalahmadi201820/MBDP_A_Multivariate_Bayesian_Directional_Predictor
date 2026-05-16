eb18700e8737df8449d0644dcf7b5869d22ba50d
jalalahmadi201820 <41103556+jalalahmadi201820@users.noreply.github.com>
2026-05-16 23:35:50 +0330

    Add files via upload

---
+++ SAMBA/config/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration module for SAMBA traffic prediction model
+"""
+
+from .model_config import ModelArgs, TrainingConfig
+
+__all__ = ['ModelArgs', 'TrainingConfig']
